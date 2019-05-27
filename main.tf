55643580e5c7935816f79a0fee25fbdfaa6efaf0
Jo Vanvoorden <jo.vanvoorden@uzleuven.be>
2019-05-27 13:38:27 +0200

    Don't define target twice

--- main.tf
+++ main.tf
@@ -29,7 +29,6 @@ resource "google_compute_global_forwarding_rule" "https" {
   count      = "${var.ssl ? 1 : 0}"
   name       = "${var.name}-https"
   target     = "${google_compute_target_https_proxy.default[count.index]}"
-  target     = "${google_compute_target_https_proxy.default[count.index]}"
   ip_address = "${google_compute_global_address.default.address}"
   port_range = "443"
   depends_on = ["google_compute_global_address.default"]
