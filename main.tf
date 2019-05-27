366bf2e0184920f246cdcc362db7a1ac39854a6f
Jo Vanvoorden <jo.vanvoorden@uzleuven.be>
2019-05-27 14:11:25 +0200

    return list of maps

--- main.tf
+++ main.tf
@@ -84,12 +84,7 @@ resource "google_compute_backend_service" "default" {
   port_name       = "${element(split(",", element(var.backend_params, count.index)), 1)}"
   protocol        = "${var.backend_protocol}"
   timeout_sec     = "${element(split(",", element(var.backend_params, count.index)), 3)}"
-  dynamic "backend" {
-    for_each = var.backends[count1.index]
-    content {
-      backend.value
-      }
-  }
+  backend         = var.backends[count.index]
   health_checks   = ["${element(google_compute_http_health_check.default.*.self_link, count.index)}"]
   security_policy = "${var.security_policy}"
   enable_cdn      = "${var.cdn}"
