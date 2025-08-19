e6078f4018b00eb444ce06e3871ea279547ff2a8
Jo Vanvoorden <jo.vanvoorden@uzleuven.be>
2025-08-19 09:07:20 +0200

    chore/SNO-10437 patch for security policy

--- main.tf
+++ main.tf
@@ -50,11 +50,12 @@ resource "google_compute_target_http_proxy" "default" {
 
 # HTTPS proxy  when ssl is true
 resource "google_compute_target_https_proxy" "default" {
-  project          = var.project
-  count            = var.ssl ? 1 : 0
-  name             = "${var.name}-https-proxy"
-  url_map          = element(compact(concat([var.url_map], google_compute_url_map.default.*.self_link)), 0)
-  ssl_certificates = compact(concat(var.ssl_certificates, google_compute_ssl_certificate.default.*.self_link))
+  project           = var.project
+  count             = var.ssl ? 1 : 0
+  name              = "${var.name}-https-proxy"
+  url_map           = element(compact(concat([var.url_map], google_compute_url_map.default.*.self_link)), 0)
+  ssl_certificates  = compact(concat(var.ssl_certificates, google_compute_ssl_certificate.default.*.self_link))
+  server_tls_policy = var.server_tls_policy
 }
 
 resource "google_compute_ssl_certificate" "default" {
